13ba5d72531f3d8d7d56586e83480474f7a13cb4
Kevin Matthews <kevmatthews3@gmail.com>
2020-07-13 22:08:11 -0400

    header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
+import Header from "./Header";
 import TaskListContextProvider from "../context/TaskListContext";
 
 import "../App.css";
@@ -11,6 +12,7 @@ const App = () => {
       <div className="container">
         <div className="app-wrapper">
           <div className="main">
+            <Header />
             <TaskList />
             <TaskForm />
           </div>
